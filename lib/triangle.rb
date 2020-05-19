e45058ac585bc566a51b1f84e85e834c1b83dc03
Joann Pan <joannlpan@gmail.com>
2020-05-19 20:34:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,11 +7,18 @@ class Triangle
   end
 
   def kind
+    if @side1 + @side2 !> == @side3 || 
+       @side2 + @side3 !> == @side1 ||
+       @side1 + @side3 !> == @side2
   end
 
 
 
-
+  class TriangleError < StandardError
+    def message
+     "you must give the get_married method an argument of an instance of the person class!"
+    end
+  end
 
 
 end
