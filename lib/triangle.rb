e77710b9ded664679ff360adeb5d39a51f1f4319
Joann Pan <joannlpan@gmail.com>
2020-05-19 21:06:59 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,6 +8,12 @@ class Triangle
   end
 
   def validate_triangle
+    sides = [@a, @b, @c]
+    if sides.each do |side|
+      side == 0 
+    raise TriangleError
+  else 
+    sides.max.pop > sides.sum
   end
 
   def kind
