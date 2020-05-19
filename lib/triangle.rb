b5b07ed9230ae8f4a37139aa19fd418fbb406253
Joann Pan <joannlpan@gmail.com>
2020-05-19 20:55:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,6 @@ class Triangle
     sides = [@a, @b, @c]
     if sides.each do |side|
       side != 0 && @a + @b > @c || @b + @c > @a || @a + @c > @b
-    else
     raise TriangleError
     end
   end
