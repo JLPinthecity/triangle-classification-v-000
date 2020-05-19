5533805e0d112ff5edff6f4694fa4dde0945cef1
Joann Pan <joannlpan@gmail.com>
2020-05-19 20:43:59 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,15 +1,16 @@
 class Triangle
-  attr_accessor :side1, :side2, :side3
+  attr_accessor :a, :b, :c
 
-  def initialize(side1, side2, side3)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
   end
 
-  def kind
-    if @side1 + @side2 !> == @side3 ||
-       @side2 + @side3 !> == @side1 ||
+    sides = [@a, @b, @c]
+
+  def validate_triangle
+    if a + b !> == c || @side2 + @side3 !> == @side1 ||
        @side1 + @side3 !> == @side2
   end
 
