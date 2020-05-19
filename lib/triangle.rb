a58c890da1de1dd0708be4bf0a2a3add8b4f57cf
Joann Pan <joannlpan@gmail.com>
2020-05-19 21:08:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,14 +8,12 @@ class Triangle
   end
 
   def validate_triangle
-    sides = [@a, @b, @c]
-    if sides.each do |side|
-      side == 0
-    raise TriangleError
-    else
-    sides.max.pop > sides.sum
-    binding.pry
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each do |side|
+      real_triangle << false if side <= 0 
+    raise TriangleError if real_triangle.include?(false)
     end
+  end
   end
 
   def kind
